9645485558ee9573646f6e9b1eb95216ae16e2cc
Louis Ouellet <louis_ouellet@hotmail.com>
2026-05-20 13:29:03 -0400

    Updating the user-menu

--- app/Views/partials/user-menu.php
+++ app/Views/partials/user-menu.php
@@ -1,4 +1,4 @@
-<div class="dropdown">
+<!-- <div class="dropdown">
     <a class="topbar-user" href="#" data-bs-toggle="dropdown" aria-expanded="false">
         <span class="topbar-avatar">
             <?= htmlspecialchars(mb_strtoupper(mb_substr($displayName, 0, 1))) ?>
@@ -31,4 +31,42 @@
             </button>
         </li>
     </ul>
+</div> -->
+<div class="app-topbar-user">
+    <div class="app-topbar-user-trigger">
+        <div class="dropdown">
+            <a class="topbar-user" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+                <div class="app-topbar-user-avatar"><?= htmlspecialchars(mb_strtoupper(mb_substr($displayName, 0, 1))) ?></div>
+                <div class="app-topbar-user-meta">
+                    <div class="app-topbar-user-label"><?= htmlspecialchars($displayName) ?></div>
+                    <div class="app-topbar-user-subtitle"><?= htmlspecialchars($user['email']) ?></div>
+                </div>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
+                <li><h6 class="dropdown-header"><?= htmlspecialchars($user['username']) ?></h6></li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                    <button class="dropdown-item js-profile-trigger" type="button">
+                        <i class="bi bi-person me-2"></i>Profile
+                    </button>
+                </li>
+                <?php
+                $canAdmin = in_array('admin', $permissions ?? [], true)
+                            || in_array('admin.access', $permissions ?? [], true);
+                ?>
+                <?php if ($canAdmin): ?>
+                <li>
+                    <a class="dropdown-item" href="/admin">
+                        <i class="bi bi-gear me-2"></i>Administration
+                    </a>
+                </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+        <div class="app-topbar-user-actions">
+            <button id="js-logout" class="app-topbar-user-action">
+                <i class="bi bi-box-arrow-right"></i>
+            </button>
+        </div>
+    </div>
 </div>
